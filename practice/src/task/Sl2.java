22a473b0472b3f7d4af4bb4d53e8782b2a66c4ad
satmika ozora <mikaozora08@gmail.com>
2023-01-10 20:11:00 +0700

    update

---
+++ practice/src/task/Sl2.java
@@ -0,0 +1,25 @@
+package task;
+
+import java.util.Scanner;
+
+public class Sl2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int r = input.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < r; i++) {
+            int temp = arr[arr.length-1];
+            for (int j = arr.length-1; j > 0; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = temp;
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
