d9098c02e0b03f65bc0b3439cd1cace2367c6e27
satmika ozora <mikaozora08@gmail.com>
2023-01-12 21:05:31 +0700

    update

--- practice/src/task/Sl2.java
+++ practice/src/task/Sl2.java
@@ -7,11 +7,12 @@ public class Sl2 {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int r = input.nextInt();
+        int mod = r % n;
         int [] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = input.nextInt();
         }
-        for (int i = 0; i < r; i++) {
+        for (int i = 0; i < mod; i++) {
             int temp = arr[arr.length-1];
             for (int j = arr.length-1; j > 0; j--) {
                 arr[j] = arr[j-1];
